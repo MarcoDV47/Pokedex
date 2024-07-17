27dd2058dc90d3e947cacb7046807276556130ac
Marco Antônio Dal Vesco <marco.dalvesco@gmail.com>
2024-07-16 22:47:20 -0300

    teste2

--- src/utils/teste.js
+++ src/utils/teste.js
@@ -1,8 +1,7 @@
-import water from "src/assets/images/water-icon";
 
 export const pokemonObj = {
     fire: "src/assets/images/fire-icon.png",
     poison: "src/assets/images/poison-icon.png",
-    water: water,
+
     grass: "src/assets/images/grass-icon.png"
 }
