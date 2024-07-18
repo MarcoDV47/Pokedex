63516e17ac668208fa29c627329e15056e40135d
Marco Antônio Dal Vesco <marco.dalvesco@gmail.com>
2024-07-18 12:24:06 -0300

    teste3

--- src/utils/teste.js
+++ src/utils/teste.js
@@ -1,5 +1,7 @@
+import poison from "../assets/images/poison-icon.png"
+
 export const pokemonObj = {
     fire: "src/assets/images/fire-icon.png",
-    poison: "src/assets/images/poison-icon.png",
+    poison: poison,
     grass: "src/assets/images/grass-icon.png"
 }
