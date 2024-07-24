3178c913010195dab379d60811c36c89bfa8a598
Marco Antônio Dal Vesco <marco.dalvesco@gmail.com>
2024-07-23 22:28:36 -0300

    fixed background color

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -34,7 +34,7 @@ const GlobalStyles = createGlobalStyle`
         gap: 1em;
         padding: 1em;
         background: url(${bgDay}) no-repeat fixed;
-        background-color: #105A94;
+        background-color: #31CDFE;
         background-size: cover;
         transition: 2s ease;
         color: #fff;
